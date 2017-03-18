addd53246b49e7906ab15628b38f4544f0274a56
Charles Ngassam <ngassamcharles@gmail.com>
2017-03-18 23:02:37 +0100

    Ajout de la condition if sur l'age

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var nom = prompt("Entre ton nom");
 var age = Number(prompt("Ton age"));
 
-alert("Bonjour "+nom+" tu as "+age);
+if(age >= 18)
+{
+alert("Bonjour "+nom+" tu as "+age+ et bravo t'es majeur!);
+}else if (age <18){
+alert("Bonjour "+nom+" tu as "+age+ et désolé t'as pas droit à l'alcool!);
+}
