7436b9ce571fdc086a9ef1b5b4222ab72bcd293b
Charles Ngassam <ngassamcharles@gmail.com>
2017-03-18 22:50:00 +0100

    Ajout

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var monObjet = {
+    nom : "charles",
+    type : "test",
+    numero : 3
+}
+
+console.log(monObjet.nom);
+console.log(monObjet.numero);
---
+++ script.js
@@ -0,0 +1,4 @@
+var nom = prompt("Entre ton nom");
+var age = Number(prompt("Ton age"));
+
+alert("Bonjour "+nom+" tu as "+age);
